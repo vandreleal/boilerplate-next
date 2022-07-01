d51e13a804423a21c0d67c2e618a8bc9d121afef
Vandre Leal <vandre.leal.candido@gmail.com>
2022-07-01 07:06:25 -0300

    build(deps): add template dependencies

---
+++ .storybook/main.js
@@ -0,0 +1,18 @@
+module.exports = {
+  stories: ["../**/*.stories.mdx", "../**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
+    "@storybook/addon-a11y",
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    "storybook-addon-next-router",
+  ],
+  framework: "@storybook/react",
+  core: {
+    builder: "@storybook/builder-webpack5",
+  },
+  babel: async options => ({
+    ...options,
+    plugins: [...options.plugins, ["@emotion"]],
+  }),
+}
--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const withPlugins = require("next-compose-plugins")
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+})
+const withPWA = require("next-pwa")
+const runtimeCaching = require("next-pwa/cache")
+
+const nextConfig = withPlugins([withBundleAnalyzer, withPWA], {
+  compiler: {
+    emotion: true,
+  },
+  pwa: {
+    dest: "public",
+    disable: process.env.NODE_ENV === "development",
+    runtimeCaching,
+  },
   reactStrictMode: true,
-}
+  swcMinify: true,
+})
 
 module.exports = nextConfig
