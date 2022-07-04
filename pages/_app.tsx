b92c64316ebfba560df3cd0c111b135aaeb2c655
Vandre Leal <vandre.leal.candido@gmail.com>
2022-07-04 10:19:16 -0300

    feat(components): add theme toggle and next-seo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,43 @@
 import type { AppProps } from "next/app"
+import { useState } from "react"
 
 import { Global, ThemeProvider } from "@emotion/react"
+import styled from "@emotion/styled"
 
 import { globalStyle } from "@/styles/global"
-import { light } from "@/styles/theme"
+import { dark, light } from "@/styles/theme"
+
+export const Container = styled.div`
+  height: 100%;
+  background-color: ${({ theme }) => theme.body.background};
+  color: ${({ theme }) => theme.body.color};
+
+  a {
+    color: ${({ theme }) => theme.body.color};
+  }
+`
 
 const App = ({ Component, pageProps }: AppProps) => {
+  const [isDarkTheme, setIsDarkTheme] = useState(false)
+
   return (
     <>
       <Global styles={globalStyle} />
-      <ThemeProvider theme={light}>
-        <Component {...pageProps} />
+      <ThemeProvider theme={isDarkTheme ? dark : light}>
+        <Container>
+          <Component {...pageProps} />
+          <button
+            style={{
+              position: "fixed",
+              bottom: "1rem",
+              right: "1rem",
+            }}
+            type="button"
+            onClick={() => setIsDarkTheme(!isDarkTheme)}
+          >
+            Change to {isDarkTheme ? "light" : "dark"} mode
+          </button>
+        </Container>
       </ThemeProvider>
     </>
   )
