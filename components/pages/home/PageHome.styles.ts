f522f2fc18bea65a3defe6a21be8375a5437819e
Vandre Leal <vandre.leal.candido@gmail.com>
2022-07-09 18:06:58 -0300

    feat(home): add dependencies and devDependencies to homepage

--- components/pages/home/PageHome.styles.ts
+++ components/pages/home/PageHome.styles.ts
@@ -6,59 +6,13 @@ export const Main = styled.main`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  height: calc(100% - 5rem);
+  height: 100%;
 `
 
-export const Footer = styled.footer`
-  display: flex;
-  flex: 1;
-  padding: 1rem 0;
-  justify-content: center;
-  align-items: center;
-`
+export const Table = styled.table`
+  min-width: min(100%, 360px);
 
-export const Grid = styled.div`
-  display: grid;
-  gap: 1rem;
-  grid-template-rows: auto;
-  grid-template-columns: repeat(auto-fit, 300px);
-  max-width: 800px;
-
-  @media (max-width: 600px) {
-    width: 100%;
-    grid-template-columns: repeat(auto-fit, 1fr);
+  td:nth-of-type(2) {
+    text-align: right;
   }
-`
-
-export const Card = styled.a`
-  padding: 1.5rem;
-  color: inherit;
-  text-align: left;
-  text-decoration: none;
-  border: 1px solid lightgrey;
-  border-radius: 10px;
-  transition: color 0.15s ease, border-color 0.15s ease;
-
-  &:hover,
-  &:focus,
-  &:active {
-    color: ${({ theme }) => theme.body.color};
-    border-color: ${({ theme }) => theme.body.color};
-  }
-
-  h2 {
-    margin: 0 0 1rem 0;
-    font-size: 1.5rem;
-  }
-
-  p {
-    margin: 0;
-    font-size: 1.25rem;
-    line-height: 1.5;
-  }
-`
-
-export const Logo = styled.span`
-  height: 1em;
-  margin-left: 0.5rem;
 `
--- components/pages/home/PageHome.tsx
+++ components/pages/home/PageHome.tsx
@@ -1,10 +1,26 @@
 import { NextSeo } from "next-seo"
-import Image from "next/image"
 import { FC } from "react"
+
+import packageJson from "package.json"
 
 import * as S from "./PageHome.styles"
 
 export const PageHome: FC = () => {
+  const { dependencies, devDependencies } = packageJson
+
+  const renderTable = (dependencies: { [s: string]: string }) => {
+    return (
+      <S.Table>
+        {Object.entries(dependencies).map(([dep, version]) => (
+          <tr key={dep}>
+            <td>{dep}</td>
+            <td>{version}</td>
+          </tr>
+        ))}
+      </S.Table>
+    )
+  }
+
   return (
     <>
       <NextSeo
@@ -12,45 +28,12 @@ export const PageHome: FC = () => {
         title="Next.js Boilerplate"
       />
       <S.Main>
-        <h1>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-        <p>
-          Get started by editing <code>pages/index.tsx</code>
-        </p>
-        <S.Grid>
-          <S.Card href="https://nextjs.org/docs">
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </S.Card>
-          <S.Card href="https://nextjs.org/learn">
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </S.Card>
-          <S.Card href="https://github.com/vercel/next.js/tree/canary/examples">
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </S.Card>
-          <S.Card href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app">
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </S.Card>
-        </S.Grid>
+        <h1>Next.js Boilerplate</h1>
+        <h2>dependencies</h2>
+        {renderTable(dependencies)}
+        <h2>devDependencies</h2>
+        {renderTable(devDependencies)}
       </S.Main>
-      <S.Footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          Powered by{" "}
-          <S.Logo>
-            <Image alt="Vercel Logo" height={16} src="/vercel.svg" width={72} />
-          </S.Logo>
-        </a>
-      </S.Footer>
     </>
   )
 }
